f8db567f62a320a41c75ed2faf745f3df45f9fc1
Puppet E.A <elias.aranda1@outlook.com>
2022-06-06 11:26:26 -0400

    Connection Terminado (creo)

--- Database/Connection.cs
+++ Database/Connection.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,22 @@ namespace ODEPA_.Database
 {
     public class Connection
     {
+        public DataTable EjecutarConsulta(SqlCommand cmd)
+        {
+            string conx = "Data Source=DESKTOP-E0BU1LI\\SQLEXPRESS;Initial Catalog=ODEPADB;Integrated Security=True";
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                cmd.Connection = new SqlConnection(conx);
+                sda.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                dt = null;
+                Console.Write(ex.Message);
+            }
+            return dt;
+        }
     }
 }
