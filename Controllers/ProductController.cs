d33519bdb9c0bf1b9ea7956a89dd378921101724
Byron Mardones <by.mardones@duocuc.cl>
2022-06-21 23:43:18 -0400

    correccion

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ODEPA_.Model;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,62 @@ namespace ODEPA_.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        private Producto pro = new Producto();
+          [HttpGet]
+          public ActionResult Index()
+          {
+              ViewBag.alerta = "info";
+              ViewBag.res = "Registrar Nuevo Producto ";
+              return View(pro.listar());
+          }
+          [HttpPost]
+          public ActionResult Index(int productID, string productName, int stock, string productType, float price, string productImage)
+          {
+              if (pro.Insertar(productID, productName, stock, productType, price, productImage))
+              {
+                  ViewBag.alerta = "success";
+                  ViewBag.res = "Producto Registrado";
+              }
+              else
+              {
+                  ViewBag.alerta = "danger";
+                  ViewBag.res = "Producto no Registrado";
+              }
+              return View(pro.listar());
+          }
+          public ActionResult Editar(int productID)
+          {
+              ViewBag.alerta = "info";
+              ViewBag.res = "Actualizar Producto";
+              return View(pro.un_registro(productID));
+          }
+          [HttpPost]
+          public ActionResult Editar(int productID, string productName, int stock, string productType, float price, string productImage)
+          {
+              if (pro.Actualizar(productID, productName, stock, productType, price, productImage))
+              {
+                  ViewBag.alerta = "success";
+                  ViewBag.res = "Datos del producto actualizados";
+              }
+              else
+              {
+                  ViewBag.alerta = "danger";
+                  ViewBag.res = "Ocurrio un error :( ";
+              }
+              return View(pro.un_registro(productID));
+          }
+          public ActionResult Eliminar(int productID)
+          {
+              if (pro.Eliminar(productID))
+              {
+                  return RedirectToAction("Index", "Producto");
+              }
+              else
+              {
+                  ViewBag.alerta = "danger";
+                  ViewBag.res = "El producto algo";
+                  return View(pro.un_registro(productID));
+              }
+          }
     }
 }
